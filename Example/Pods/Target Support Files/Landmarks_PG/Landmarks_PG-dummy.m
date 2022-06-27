dd5e5103b95c07cbbf7eb4827e74fae14cfb2b5a
Gannamani Praveen <Praveen@macs-MacBook-Pro.local>
2022-06-27 12:01:18 +0530

    Initial commit with cocoapodws

---
+++ Example/Pods/Target Support Files/Landmarks_PG/Landmarks_PG-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Landmarks_PG : NSObject
+@end
+@implementation PodsDummy_Landmarks_PG
+@end
---
+++ Example/Pods/Target Support Files/Pods-Landmarks_PG_Tests/Pods-Landmarks_PG_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Landmarks_PG_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Landmarks_PG_Tests
+@end
